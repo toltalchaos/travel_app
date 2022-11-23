b27514e7a0a9bfd6c8905e6807189fcf6ff3a326
toltalchaos <59936738+toltalchaos@users.noreply.github.com>
2022-11-22 20:12:33 -0700

    added new pages and brief event survey.

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,5 +1,20 @@
 <script>
+	import NavButton from '../../components/navButton.svelte';
+	import GlobalTravelObject from '../../models/store';
 
+  function changeGlobalValue(value) {
+		GlobalTravelObject.update((current) => {
+			// return current.setEventSurvey(value);
+		});
+	}
+
+	function onChange(e) {
+		document.getElementsByName(buttonGroup).forEach((element) => {
+			element.parentElement.classList.remove('checked');
+		});
+		e.target.parentElement.classList.add('checked');
+		changeGlobalValue({ [buttonGroup]: (selected === 'true') });
+	}
 </script>
 
 <main>
