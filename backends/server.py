a96b81130da063245e7faa05416287d7c485bde0
Braydon Tol <braydon.tol@drivewyze.com>
2022-12-15 12:59:02 -0700

    add server

---
+++ backends/server.py
@@ -0,0 +1,3 @@
+# https://skyscannerpartnersupport.zendesk.com/hc/en-us/articles/4687297411869-Is-it-possible-to-use-Skyscanner-s-API-directly-via-an-AJAX-call-if-my-website-is-using-HTTPS-
+
+# need to impliment some flask-like backend to accept requests from the frontend and subsequently make calls to APIs that do not handle CORS
